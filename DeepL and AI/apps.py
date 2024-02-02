c2651b4da21ec47628bed2c3657b18e244e75369
Hasan <hasan.error.code@gmail.com>
2024-02-02 12:24:21 +0600

    create project

---
+++ DeepL and AI/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DeeplAndAiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'DeepL_and_AI'
---
+++ LIVE_Courses/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LiveCoursesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LIVE_Courses'
