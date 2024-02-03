12745b94ee41d6d21fefb4238ca94304fe36667d
Hasan <hasan.error.code@gmail.com>
2024-02-03 11:47:46 +0600

    common code setup

--- Big_Data/views.py
+++ Big_Data/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'Big_Data/big_data.html')
+def common(request):
+    return render(request, 'common_code/base.html')
